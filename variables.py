47d495ab18ef9451c261e8d51affff362ca3d766
Karl-Markus Sangernebo <karlmarkus.sangernebo@gmail.com>
2024-10-16 13:05:22 +0300

    muutujad ja test

---
+++ variables.py
@@ -0,0 +1,17 @@
+# 1. Muutuja word väärtusega "spider"
+word = "spider"
+
+# 2. Muutuja thousand väärtusega "1000"
+thousand = "1000"
+
+# 3. Muutuja ten väärtusega 10
+ten = 10
+
+# 4. Muutuja first_half väärtusega "full"
+first_half = "full"
+
+# 5. Muutuja second_half väärtusega "moon"
+second_half = "moon"
+
+# 6. Muutuja complete väärtusega "full moon", kasutades first_half ja second_half
+complete = first_half + " " + second_half
